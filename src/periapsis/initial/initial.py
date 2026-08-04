e76c06b4e0b2b62fb8871617532570e6745dfa0d
Matthew Whitaker <matthew.whitaker@utah.edu>
2026-08-04 16:11:17 -0600

    Fix astrometry initial guess bounds

--- src/periapsis/initial/initial.py
+++ src/periapsis/initial/initial.py
@@ -1,7 +1,7 @@
 from abc import abstractmethod, ABC
 import numpy as np
 from periapsis.data import Data
-from periapsis.prior import Prior
+from periapsis.prior import Prior, FixedPrior
 
 class InitialGuess(ABC):
     """Class for obtaining an intial guess on sampled parameters"""
@@ -9,6 +9,7 @@ class InitialGuess(ABC):
         self.data = data
         self.rng = rng
         self.priors = priors
+        self.fixed_prior_params = {name: prior for name, prior in priors.items() if isinstance(prior, FixedPrior)}
 
     @abstractmethod
     def get_initial_guess(self, param_order, nwalkers: int) -> np.ndarray:
